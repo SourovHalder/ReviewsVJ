fb422a5d44d824244c3bb9b3939f4613bf94cb75
sourov halder <sourovhalder97@gmail.com>
2022-03-01 22:55:37 +0600

    final

--- app.js
+++ app.js
@@ -51,11 +51,53 @@ const randomBtn = document.querySelector(".random-btn");
 
 //set starting item
 
-let currentItem = 2;
+let currentItem = 0;
 
 // load initial item
-
+ 
 window.addEventListener("DOMContentLoaded",function(){
+ showPerson(currentItem);
+
+});
+
+//show person based on item
+
+function showPerson(){
   const item =reviews[currentItem];
-  img.src =item.img
+  img.src =item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+//show next person
+
+nextBtn.addEventListener("click",function(){
+  currentItem++;
+  if(currentItem > reviews.length - 1 ){
+    currentItem = 0;
+
+
+  }
+  showPerson(currentItem);
+});
+
+
+prevBtn.addEventListener("click",function(){
+  currentItem--;
+  if(currentItem < 0 ){
+    currentItem = reviews.length - 1 ;
+    
+
+  }
+  showPerson(currentItem);
+});
+
+//show random person
+
+randomBtn.addEventListener("click", function () {
+  console.log("hello");
+
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
 });
